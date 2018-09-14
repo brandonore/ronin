55103c6a7d3f601ad672bed6152b8208ef7ee7f0
Brandon Ore <y2ktheory@gmail.com>
2018-09-13 19:17:34 -0600

    Fixed img upload & size issues. Added new inputs

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,49 @@
+/*---------------------------------------
+* Requires and variables
+* ---------------------------------------*/
 const ElectronTitlebarWindows = require('electron-titlebar-windows');
 const titlebar = new ElectronTitlebarWindows({draggable: true, backgroundColor: '#03c9a9'});
 const jsPDF = require('jspdf');
 let imgData = '';
+let width, height;
+let pdf = new jsPDF('p', 'mm', 'a4');
 
 titlebar.appendTo(document.getElementById('title-bar'));
-
 titlebar.on('close', function(e) {
     console.log('close');
 });
 
-let pdf = new jsPDF();
-let width = pdf.internal.pageSize.width;
-let height = pdf.internal.pageSize.height;
-let h = 500;
-let aspectwidth = (height - h)*(9/16);
-
+/*---------------------------------------
+* Handle clicks
+* ---------------------------------------*/
+//generate pdf on button click
 $('#gen').on('click', () => {
     printPDF();
 })
-
-const printPDF = () => {
-    // pdf.text(15, 10, 'Yay pdf!');
-    pdf.addImage(imgData, 'PNG', 5, 5, aspectwidth, (height - h));
-    pdf.save(`${$('#inv-num').val()}.pdf`);
-    removeImg();
-}
-
-//img upload 
-document.getElementById('file').addEventListener('change', readURL, true);
 
 //clear img
 $('#remove-img').on('click', () => {
     removeImg();
 })
+
+//hide default file input button, replace with icon
+$('.fa-upload').on('click', () => {
+    $('input[type="file"]').trigger('click');
+})
+
+//img upload 
+document.getElementById('file').addEventListener('change', readURL, true);
+
+/*---------------------------------------
+* Functions
+* ---------------------------------------*/
+// write data to pdf document
+const printPDF = () => {
+    // pdf.text(15, 10, 'Yay pdf!');
+    pdf.addImage(imgData, 'PNG', 10, 5, width * 0.05, height * 0.05);
+    pdf.save(`${$('#inv-num').val()}.pdf`);
+    removeImg();
+}
 
 //select img and set background of container
 function readURL() {
@@ -41,6 +52,13 @@ function readURL() {
     reader.onloadend = () => {
         document.getElementById('img-select').style.backgroundImage = `url(${reader.result})`;
         imgData = reader.result;
+        let image = new Image();
+        image.src = reader.result;
+        image.onload = () => {
+            console.log(image.width, image.height);
+            height = image.height;
+            width = image.width;
+        }
     }
     if(file) {
         reader.readAsDataURL(file);
@@ -57,10 +75,5 @@ function removeImg() {
     $('#img-select').css({'border': 'solid 1px #bdc3c7', 'background-color': '#F8F9F9'});
     $('.fa-upload').css('display', 'block');
     $('#remove-img').css('display', 'none');
-    pdf = new jsPDF();
+    pdf = new jsPDF('p', 'mm', 'a4');
 }
-
-//hide default file input button, replace with icon
-$('.fa-upload').on('click', () => {
-    $('input[type="file"]').trigger('click');
-})
